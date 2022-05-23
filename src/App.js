01bae323331bd1949626fd9c045681a7cb6f50f5
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-05-24 00:38:39 +0600

    React router add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
 import Form from "./components/common/Form";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/common/Home";
+import "./App.css";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { app } from "./Firebase/firebase-config";
 import {
@@ -13,48 +14,55 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  let navigate = useNavigate();
+
   const handleAction = (id) => {
     console.log(email);
     const authentication = getAuth();
     if (id === 2) {
-      createUserWithEmailAndPassword(authentication, email, password)
-        .then((response) => console.log(response))
+      createUserWithEmailAndPassword(authentication, email, password) //It will take three parameters – authentication, the email state, and the password state
+        .then((response) => {
+          navigate("/home");
+          // sessionStorage.setItem(
+          //   "Auth Token",
+          //   response._tokenResponse.refreshToken
+          // );
+          console.log(response);
+        })
         .catch((error) => {
           console.log(error);
         });
-      //It will take three parameters – authentication, the email state, and the password state
     }
   };
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/login"
-            element={
-              <Form
-                title="Login"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(1)}
-              />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Form
-                title="Register"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(2)}
-              />
-            }
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <Form
+              title="Login"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(1)}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Form
+              title="Register"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(2)}
+            />
+          }
+        />
+        {/* <Route path="/home" element={<Home />} /> */}
+      </Routes>
+    </div>
   );
 }
 
