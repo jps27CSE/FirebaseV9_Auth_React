4f67e92a6fe2d3592b0319d6b9683c7c09e92374
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-05-26 02:24:59 +0600

    added toast notification, logout and login functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Form from "./components/common/Form";
 import Home from "./components/common/Home";
 import "./App.css";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { app } from "./Firebase/firebase-config";
 import {
   getAuth,
@@ -10,33 +10,67 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   let navigate = useNavigate();
 
+  useEffect(() => {
+    let authToken = sessionStorage.getItem("Auth Token");
+
+    if (authToken) {
+      navigate("/home");
+    }
+  }, []);
+
   const handleAction = (id) => {
     console.log(email);
     const authentication = getAuth();
+
+    if (id === 1) {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          if (error.code === "auth/wrong-password") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/user-not-found") {
+            toast.error("Please check the Email");
+          }
+        });
+    }
+
     if (id === 2) {
       createUserWithEmailAndPassword(authentication, email, password) //It will take three parameters – authentication, the email state, and the password state
         .then((response) => {
           navigate("/home");
-          // sessionStorage.setItem(
-          //   "Auth Token",
-          //   response._tokenResponse.refreshToken
-          // );
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
           console.log(response);
         })
         .catch((error) => {
-          console.log(error);
+          if (error.code === "auth/email-already-in-use") {
+            toast.error("Email Already in Use");
+          }
         });
     }
   };
 
   return (
     <div className="App">
+      <ToastContainer />
       <Routes>
         <Route
           path="/login"
@@ -60,7 +94,7 @@ function App() {
             />
           }
         />
-        {/* <Route path="/home" element={<Home />} /> */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,4 +1,29 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const handleLogout = () => {
+    sessionStorage.removeItem("Auth Token");
+    navigate("/login");
+  };
+
+  let navigate = useNavigate();
+  useEffect(() => {
+    let authToken = sessionStorage.getItem("Auth Token");
+
+    if (authToken) {
+      navigate("/home");
+    }
+    if (!authToken) {
+      navigate("/login");
+    }
+  }, []);
+
+  return (
+    <div>
+      Home
+      <button onClick={handleLogout}>Log out</button>
+    </div>
+  );
 };
 export default Home;
